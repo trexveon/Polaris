04063b763e8523a6561389ba57d3ece2d9a2b825
EvertonVargas <dev2@human2be.com.br>
2019-09-26 23:21:04 -0300

    versão final

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,37 @@
-const 
+const
 gulp = require('gulp'),
 sass = require('gulp-sass'),
 sourcemaps = require('gulp-sourcemaps'),
 browserSync = require('browser-sync').create(),
 watch = require('gulp-watch');
 
+
 const path = {
     srcPath: 'dev/assets/css/scss/',
-    distPath: 'dev/assets/css/'
+    distPath: 'dev/assets/css/',
+    // jquery: 'node_modules/jQuery/tmp/jquery.js',
+    popper: 'node_modules//popper.js/dist/umd/popper.min.js',
+    bootstrapjs: 'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    bootstrapcss: 'node_modules/bootstrap//dist/css/bootstrap.min.css',
+    js: 'dev/assets/js',
+    css: 'dev/assets/css'
   };
 
-  gulp.task('browserSync', function() {
+gulp.task('js',function(){//enviar arquivos js do npm para a pasta js
+  return gulp.src([path.popper,path.bootstrapjs])
+  .pipe(gulp.dest(path.js))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('css',function(){//enviar arquivos css do npm para a pasta css
+  return gulp.src([path.bootstrapcss])
+  .pipe(gulp.dest(path.css))
+  .pipe(browserSync.stream());
+});
+
+
+
+gulp.task('browserSync', function() {
     browserSync.init({ //inicia o server do browsersync
       server: {
         baseDir: 'dev/', //define o diretório base
@@ -19,10 +40,10 @@ const path = {
       startPath: 'index.html', //define qual é o arquivo que abrirá como padrão quando ele iniciar
     })
   });
-  
+
   gulp.task('sass', function(){
     //a marcação define que iremos pegar todos os arquivos SCSS e Sass da pasta src/sass, inclusive subpastas e seu conteúdo se houverem
-    return gulp.src(path.srcPath+'**/*.+(scss|sass)')
+    return gulp.src(path.srcPath+'/style.+(scss|sass)') //'**/*.+(scss|sass)')
       .pipe(sourcemaps.init()) //iniciamos o sourcemap para gravar o MAP para debuging
       .pipe(sass({ //iniciamos o modulo do Sass
         outputStyle: 'compressed' //adicionamos a opção para que o produto final seja comprimido/minificado
@@ -34,12 +55,12 @@ const path = {
       }));
   });
 
-  
-  
-  gulp.task('watch', function () { 
-    gulp.watch(path.srcPath+'**/*.scss', ['sass', 'browserSync']); 
+
+  gulp.task('watch', function () {
+    gulp.watch(path.srcPath+'/style.scss', ['sass', 'browserSync']);
     gulp.watch(['dev/**/*.html','dev/assets/js/**/*.js','**/*.php']).on('change', browserSync.reload);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss'],['sass']);
   });
 
-  gulp.task('default', ['sass', 'browserSync', 'watch']);
+  gulp.task('default', ['sass', 'js','browserSync', 'watch', 'css']);
 
