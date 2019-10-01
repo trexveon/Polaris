54d5442e90f0d7575ff0229f54e785429c409650
Everton <evertoniee@yahoo.com.br>
2019-10-01 18:25:32 -0300

    plantas

--- dev/assets/js/script.js
+++ dev/assets/js/script.js
@@ -8,4 +8,31 @@
         prevArrow: $('.prev'),
         nextArrow: $('.prox')
       });
+
+      var scrollItem = document.querySelector('.topHeader');
+      var bottom = document.querySelector('.bott');
+      var logo = document.querySelector('.iconz');
+
+      function scroll () {
+          if(window.pageYOffset>100){
+            scrollItem.classList.add('scroll');
+            bottom.classList.remove('btn-outline-light');
+            bottom.classList.add('btn-outline-dark');
+            logo.src = 'assets/img/ZECONOriginal.png';
+
+          }else{
+            scrollItem.classList.remove('scroll');
+            bottom.classList.add('btn-outline-light');
+            bottom.classList.remove('btn-outline-dark');
+            logo.src = 'assets/img/ZECON.png';
+          }
+       }
+
+       window.onscroll = scroll;
+
+
+       
+
+       
   });
+
