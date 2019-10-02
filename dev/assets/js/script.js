54a8ac12600ef5f2d8445d5a14c10e768dc0d46a
Everton <evertoniee@yahoo.com.br>
2019-10-02 18:22:20 -0300

    formulario e redes sociais

--- dev/assets/js/script.js
+++ dev/assets/js/script.js
@@ -9,19 +9,36 @@
         nextArrow: $('.prox')
       });
 
+      $('.itens').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: true,
+        prevArrow: $('.prevtempo'),
+        nextArrow: $('.proxtempo'),
+
+        responsive: [{
+            breakpoint: 700,
+            settings: {
+              slidesToShow: 4
+            }
+          }]
+      });
+
       var scrollItem = document.querySelector('.topHeader');
       var bottom = document.querySelector('.bott');
       var logo = document.querySelector('.iconz');
 
       function scroll () {
           if(window.pageYOffset>100){
-            scrollItem.classList.add('scroll');
+            scrollItem.classList.add('scrolll');
             bottom.classList.remove('btn-outline-light');
             bottom.classList.add('btn-outline-dark');
             logo.src = 'assets/img/ZECONOriginal.png';
 
           }else{
-            scrollItem.classList.remove('scroll');
+            scrollItem.classList.remove('scrolll');
             bottom.classList.add('btn-outline-light');
             bottom.classList.remove('btn-outline-dark');
             logo.src = 'assets/img/ZECON.png';
